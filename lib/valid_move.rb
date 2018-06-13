a260b338aa241924c953ac228a2cc471260520f5
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-13 20:31:23 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,7 +8,7 @@ def position_taken?(board,index)
 end
 
 def valid_move?(board,index)
-  if index.between?(0, 8) == true && position_taken?(board,index) == false
+  if index.between?(0, 8) && !position_taken?(board,index)
     true
   else
     false
